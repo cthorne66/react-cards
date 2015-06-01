495c07a2ac2c1eff45f863116a3d907b157be533
Chris Thorne <cthorne@cthorne.com>
2015-06-01 00:06:18 -0500

    Adding more drag events

--- src/components/CardsPage/CardsPage.js
+++ src/components/CardsPage/CardsPage.js
@@ -13,11 +13,14 @@ class CardsPage extends React.Component{
     this.state = {
       cards: [{
         title: 'Card A',
-        inConfig: false
+        inConfig: false,
+        isDragStart: false
       }, {
         title: 'Card B',
-        inConfig: false
-      }]
+        inConfig: false,
+        isDragStart: false
+      }],
+      dragSrcEl: null
     };
   };
 
@@ -25,19 +28,108 @@ class CardsPage extends React.Component{
     onSetTitle: PropTypes.func.isRequired
   };
 
-  handleShowBack(id){
+  handleShowBack(id, event){
+    console.log(arguments);
     event.preventDefault();
     var cards = this.state.cards;
     cards[id].inConfig = true;
     this.setState({cards: cards});
   };
 
-  handleShowFront(id){
+  handleShowFront(id, event){
     event.preventDefault();
     var cards = this.state.cards;
     cards[id].inConfig = false;
     this.setState({cards: cards});
   };
+
+  handleDragStart(id, event) {
+    event.preventDefault();
+    console.log('dragstart');
+
+    var cards = this.state.cards;
+    cards[id].isDragStart = true;
+    this.setState({
+      cards: cards,
+      dragSrcEl: this
+    });
+
+    event.dataTransfer.effectAllowed = 'move';
+    event.dataTransfer.setData('text/html', this.innerHTML);
+  };
+
+  handleDragEnter(id, event) {
+    console.log('dragenter');
+
+    var cards = this.state.cards;
+    cards[id].isDragEnter = true;
+    this.setState({
+      cards: cards
+    });
+  };
+
+  handleDragOver(id, event) {
+    console.log('dragover');
+    event.preventDefault();
+
+    event.dataTransfer.dropEffect = 'move'; // See the section on the DataTransfer object.
+  };
+
+  // elm.bind('dragenter', function() {
+  //   console.log('dragenter');
+  //   elm.addClass('over');
+  // });
+
+  // elm.bind('dragover', function(e) {
+  //   console.log('dragover');
+  //   if (e.preventDefault) {
+  //     e.preventDefault(); // Necessary. Allows us to drop.
+  //   }
+
+  //   var orig = e.originalEvent;
+
+  //   orig.dataTransfer.dropEffect = 'move'; // See the section on the DataTransfer object.
+
+  //   return false;
+  // });
+
+  // elm.bind('dragleave', function() {
+  //   console.log('dragleave');
+  //   elm.removeClass('over');
+  // });
+
+  // elm.bind('dragend', function() {
+  //   console.log('dragend');
+  //   $('.card-container').removeClass('over');
+  //   this.style.opacity = '1';
+  // });
+
+  // // $(elm.parent()[0]).bind('drop', function(e){
+  // elm.bind('drop', function(e) {
+  //   console.log('drop');
+
+  //   // this/e.target is current target element.
+
+  //   if (e.stopPropagation) {
+  //     e.stopPropagation(); // Stops some browsers from redirecting.
+  //   }
+
+  //   var orig = e.originalEvent;
+
+  //   // Don't do anything if dropping the same column we're dragging.
+  //   if (dragSrcEl != this) {
+  //     // Set the source column's HTML to the HTML of the column we dropped on.
+  //     dragSrcEl.innerHTML = this.innerHTML;
+  //     this.innerHTML = orig.dataTransfer.getData('text/html');
+  //   }
+
+  //   elm.removeClass('over');
+
+  //   // $compile(elm)(scope);
+
+  //   return false;
+
+  // });
 
   render() {
     let title = 'Cards';
@@ -45,18 +137,28 @@ class CardsPage extends React.Component{
 
 
     let cardNodes = this.state.cards.map(function (card, i) {
-          let inConfig = this.state.cards[i].inConfig;
+          let config = this.state.cards[i];
 
           let classes = classNames({
             'card-container': true,
-            'activate': inConfig
+            'activate': config.inConfig,
+            'over': config.isDragEnter
           });
 
-          let draggable = (inConfig) ? false : true;
+          let styles = {
+            'opacity': config.isDragStart ? '0.4' : '1'
+          }
+
+          let draggable = (config.inConfig) ? false : true;
 
 
           return (
-            <div className={classes} draggable={draggable} key={i}>
+            <div key={i} className={classes} style={styles} 
+              draggable={draggable} 
+              onDragStart={this.handleDragStart.bind(this, i)} 
+              onDragEnter={this.handleDragEnter.bind(this, i)}
+              onDragOver={this.handleDragOver.bind(this, i)}>
+
               <div className="card-front">
                 <div>{card.title} <a href="" className="config-activate" onClick={this.handleShowBack.bind(this, i)}>Flip</a></div>
                 <div>body</div>
